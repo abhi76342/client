f27ccdb33f7204c9ebc81e15b2d3748f709e7e53
ABHISHEK KUMAR <akraj844@gmai.com>
2024-05-03 04:15:26 +0530

    Initial-Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+// App.js
+import React from 'react';
 import './App.css';
 
-function App() {
+function Portfolio() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="portfolio">
+      <h1>My Portfolio</h1>
+      <p>Welcome to my portfolio website!</p>
     </div>
   );
 }
 
-export default App;
+export default Portfolio;
